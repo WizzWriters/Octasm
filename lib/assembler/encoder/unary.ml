4f6222f1c9176d792533bb8163536b67646e37f7
MusicFreak456 <czarek44564@gmail.com>
2024-03-07 21:35:43 +0100

    Add skp and sknp instructions

--- lib/assembler/encoder/unary.ml
+++ lib/assembler/encoder/unary.ml
@@ -30,26 +30,36 @@ let encode_call_instruction arg_expr =
   let (upper, lower) = split_int argument_value in
   [upper lor 0x20; lower]
 
-let encode_shift_right_instruction arg_expr =
+let encode_general_purpose_register_instruction arg_expr callback =
   let register_expr = get_register_expr arg_expr in
   match get_register_from_register_expr register_expr with
-  | GeneralPurpose x ->
-    [ 0x80 lor x; (x lsl 4) lor 0x06 ]
+  | GeneralPurpose x -> callback x
   | _ -> Assembler_error.throw @@ Assembler_error.BadRegister register_expr
 
+let encode_shift_right_instruction arg_expr =
+  encode_general_purpose_register_instruction arg_expr
+  (fun x -> [ 0x80 lor x; (x lsl 4) lor 0x06 ])
+
 let encode_shift_left_instruction arg_expr =
-  let register_expr = get_register_expr arg_expr in
-  match get_register_from_register_expr register_expr with
-  | GeneralPurpose x ->
-    [ 0x80 lor x; (x lsl 4) lor 0x0E ]
-  | _ -> Assembler_error.throw @@ Assembler_error.BadRegister register_expr
+  encode_general_purpose_register_instruction arg_expr
+  (fun x -> [ 0x80 lor x; (x lsl 4) lor 0x0E ])
+
+let encode_skip_instruction arg_expr =
+  encode_general_purpose_register_instruction arg_expr
+  (fun x -> [ 0xE0 lor x; 0x9E ])
+
+let encode_do_not_skip_instruction arg_expr =
+  encode_general_purpose_register_instruction arg_expr
+  (fun x -> [ 0xE0 lor x; 0xA1 ])
 
 let unary_instruction_list = [
   ("sys", encode_syscall_instruction);
   ("jp", encode_jump_instruction);
   ("call", encode_call_instruction);
   ("shr", encode_shift_right_instruction);
-  ("shl", encode_shift_left_instruction)
+  ("shl", encode_shift_left_instruction);
+  ("skp", encode_skip_instruction);
+  ("sknp", encode_do_not_skip_instruction)
 ]
 
 let unary_instruction_lookup_table =
