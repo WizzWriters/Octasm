7c0bd49245fab008dafa060e94db781410cc5faf
MusicFreak456 <czarek44564@gmail.com>
2024-03-11 00:05:14 +0100

    Add some arythmetic instructions

--- lib/assembler/encoder/binary.ml
+++ lib/assembler/encoder/binary.ml
@@ -46,11 +46,42 @@ let encode_skip_neq_instruction arg1 arg2 =
   | _,_ ->
     Assembler_error.throw @@ Assembler_error.TypeError arg1
 
-let encode_or_instruction arg1 arg2 =
+let encode_arythmetic_instruction inst_number arg1 arg2 =
   match arg1, arg2 with
   | RegisterExpr reg1_expr, RegisterExpr reg2_expr ->
-    let encode_or reg1 reg2 = [0x80 lor reg1; (reg2 lsl 4) lor 0x01] in
-    encode_reg_reg_instruction reg1_expr reg2_expr encode_or
+    let encode reg1 reg2 = [0x80 lor reg1; (reg2 lsl 4) lor inst_number] in
+    encode_reg_reg_instruction reg1_expr reg2_expr encode
+  | RegisterExpr _, _ ->
+    Assembler_error.throw @@ Assembler_error.TypeError arg2
+  | _,_ ->
+    Assembler_error.throw @@ Assembler_error.TypeError arg1
+
+let encode_or_instruction = encode_arythmetic_instruction 0x01
+let encode_and_instruction = encode_arythmetic_instruction 0x02
+let encode_xor_instruction = encode_arythmetic_instruction 0x03
+let encode_sub_instruction = encode_arythmetic_instruction 0x05
+let encode_subn_instruction = encode_arythmetic_instruction 0x07
+
+let encode_add_reg_reg_instruction reg1_expr reg2_expr =
+  let register1 = get_register_from_register_expr reg1_expr in
+  let register2 = get_register_from_register_expr reg2_expr in
+  match register1, register2 with
+  | GeneralPurpose regnum1, GeneralPurpose regnum2 ->
+    [0x80 lor regnum1; (regnum2 lsl 4) lor 0x04]
+  | LongRegister, GeneralPurpose regnum ->
+    [0xF0 lor regnum; 0x1E]
+  | LongRegister, _ | GeneralPurpose _, _ ->
+    Assembler_error.throw @@ Assembler_error.BadRegister reg2_expr
+  | _,_ ->
+    Assembler_error.throw @@ Assembler_error.BadRegister reg1_expr
+
+let encode_add_instruction arg1 arg2 =
+  match arg1, arg2 with
+  | RegisterExpr reg_expr, ConstExpr const_expr ->
+    let encode_add reg byte = [0x70 lor reg; byte] in
+    encode_reg_byte_instruction reg_expr const_expr encode_add
+  | RegisterExpr reg1_expr, RegisterExpr reg2_expr ->
+    encode_add_reg_reg_instruction reg1_expr reg2_expr
   | RegisterExpr _, _ ->
     Assembler_error.throw @@ Assembler_error.TypeError arg2
   | _,_ ->
@@ -59,7 +90,12 @@ let encode_or_instruction arg1 arg2 =
 let binary_instruction_list = [
   ("se", encode_skip_eq_instruction);
   ("sne", encode_skip_neq_instruction);
-  ("or", encode_or_instruction)
+  ("or", encode_or_instruction);
+  ("and", encode_and_instruction);
+  ("xor", encode_xor_instruction);
+  ("add", encode_add_instruction);
+  ("sub", encode_sub_instruction);
+  ("subn", encode_subn_instruction)
 ]
 
 let binary_instruction_lookup_table =
